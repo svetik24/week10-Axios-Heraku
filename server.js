e838845dabeb44e2a904ea514c09ca61d9cae077
ees-ja perekonnanimi <svetik24@hotmail.com>
2024-05-01 08:22:12 +0300

    merging two projects

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const axios = require("axios");
-
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true }));
 
-
-app.get("/", (req, res)=>{ 
+app.get('/', (req, res) => {
     let url = "https://api.themoviedb.org/3/movie/787699?api_key=87a32ef55335ba35e416797010a0e286";
 axios.get(url)
 .then(response =>{
-
 let data = response.data;
 let releaseDate = new Date(data.release_date).getFullYear();
 let genresToDisplay ="";
@@ -23,9 +21,6 @@ data.genres.forEach(genre => {
 let genresUpdated = genresToDisplay.slice(0, -2) + ".";
 let posterUrl =`https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
 
-
-
-
 let currentYear = new Date().getFullYear(); 
 
     res.render("index", {
@@ -36,9 +31,64 @@ let currentYear = new Date().getFullYear();
     poster: posterUrl
     });
 });
-
-
 });
+app.get("/search", (req, res)=>{ 
+    res.render("search", { movieDetails:"" });
+    });
+
+app.post("/search", (req, res) =>{
+    let userMovieTitle = req.body.movieTitle;
+    let movieUrl =`https://api.themoviedb.org/3/search/movie?api_key=87a32ef55335ba35e416797010a0e286&query=${userMovieTitle}`;
+    let genresUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=87a32ef55335ba35e416797010a0e286&language=en-US";
+    
+    let endpoints = [movieUrl, genresUrl];
+    
+    axios.all(endpoints.map((endpoint)  => axios.get(endpoint)))
+    .then(axios.spread((movie, genres) =>{ 
+    
+    const [movieRaw] = movie.data.results;
+    
+    let movieGenreIds = movieRaw.genre_ids;
+    let movieGenres = genres.data.genres;
+    
+    let movieGenresArray =  [];
+    
+    for(let i = 0; i < movieGenreIds.length; i++){
+    for(let j = 0; j < movieGenres.length; j++) { 
+    if(movieGenreIds[i] === movieGenres[j].id) {
+        movieGenresArray.push(movieGenres[j].name);
+    
+     }
+    }
+    }
+    let genresToDisplay = "";
+    movieGenresArray.forEach(genre => {
+        genresToDisplay = genresToDisplay+ `${genre}, `;
+    });
+    genresToDisplay = genresToDisplay.slice(0, -2) + ".";
+    
+    
+        let movieData = { 
+            title: movieRaw.title,
+            year: new Date(movieRaw.release_date).getFullYear(),
+            genres: genresToDisplay,
+            overview: movieRaw.overview,
+            posterUrl: `https://image.tmdb.org/t/p/w500${movieRaw.poster_path}`
+    
+         
+            };
+    
+            res.render("search", {movieDetails: movieData});
+    
+    }));
+    
+    
+    
+    });
+
+
+
+
 
 app.listen(process.env.PORT || 3000, () => { 
 console.log("Server is running on Port 3000");
